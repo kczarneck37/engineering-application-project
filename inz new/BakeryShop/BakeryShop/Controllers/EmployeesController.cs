69cef758e922ac20606fdc100eac8feca21adb39
Karol Czarnecki <karol.czarnecki37@o2.pl>
2018-12-15 14:38:45 +0100

    New actions for employee.

--- inz new/BakeryShop/BakeryShop/Controllers/EmployeesController.cs
+++ inz new/BakeryShop/BakeryShop/Controllers/EmployeesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using BakeryShop.Models;
@@ -30,8 +32,76 @@ namespace BakeryShop.Controllers
             {
                 _context.Employees.Add(employee);
                 _context.SaveChanges();
+                return RedirectToAction("Index");
             }
             return View(employee);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var employee = _context.Employees.SingleOrDefault(e => e.EmployeeId == id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(employee).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var employee = _context.Employees.SingleOrDefault(e => e.EmployeeId == id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var employee = _context.Employees.SingleOrDefault(e => e.EmployeeId == id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var employee = _context.Employees.SingleOrDefault(x => x.EmployeeId == id);
+            _context.Employees.Remove(employee ?? throw new InvalidOperationException());
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
