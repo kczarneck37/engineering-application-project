a29e6f36baabbaa780d403223fe8ef56bd5d8ad8
Karol Czarnecki <karol.czarnecki37@o2.pl>
2018-12-28 17:36:52 +0100

    Files update

---
+++ inz new/BakeryShop/BakeryShop/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryShop.Models;
+
+namespace BakeryShop.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(User userModel)
+        {
+            using (BakeryContext BakeryContext = new BakeryContext())
+            {
+                if (BakeryContext.Users.Any(x => x.UserName == userModel.UserName))
+                {
+                    ViewBag.DuplicateMessage = "Username already exist.";
+                    return View("AddOrEdit", userModel);
+                }
+
+                BakeryContext.Users.Add(userModel);
+                BakeryContext.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("AddOrEdit", new User());
+        }
+    }
+}
---
+++ inz new/BakeryShop/BakeryShop/Migrations/201812281603234_Registration.cs
@@ -0,0 +1,28 @@
+namespace BakeryShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Registration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserID = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Password = c.String(),
+                        isAdmin = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
